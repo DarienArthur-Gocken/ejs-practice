ab0e013bda1f42d18950de4c8b4ea4000b3a9585
Darien Arthur-Gocken <233949874+DarienArthur-Gocken@users.noreply.github.com>
2025-11-04 11:27:34 -0800

    person object added

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('home', {name : 'Donald Duck', occupation: 'plumber'});
+    const person = {name : 'Donald Duck', occupation: 'plumber'};
+    res.render('home', {person});
 })
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
