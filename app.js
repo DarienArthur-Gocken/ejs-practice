e488cf31e653541f7c1fac4621e6f3056452826a
Connor <connorjohnhughes@gmail.com>
2025-11-04 11:34:05 -0800

    Added friends route

--- app.js
+++ app.js
@@ -9,5 +9,9 @@ app.get('/', (req, res) => {
     const person = {name : 'Donald Duck', occupation: 'plumber'};
     res.render('home', {person});
 })
+app.get('/friends', (req, res) => {
+    const friends = ["Mickey", "Goofy", "Minnie", "Daisey"];
+    res.render('friends', {friends});
+})
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
